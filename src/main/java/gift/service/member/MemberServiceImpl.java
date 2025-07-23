c544cda0bfc376f0289f2a08c8b76d79b57eade0
eastgyu11 <eastgyu11>
2025-07-23 13:17:16 +0900

    style: 불필요한 주석 제거

--- src/main/java/gift/service/member/MemberServiceImpl.java
+++ src/main/java/gift/service/member/MemberServiceImpl.java
@@ -78,7 +78,6 @@ public class MemberServiceImpl implements MemberService {
     Member member = repository.findById(id)
         .orElseThrow(() -> new MemberNotFoundException("member가 없습니다"));
     member.update(requestDto.getEmail(), requestDto.getPassword());
-//    repository.save(member);
     return new MemberResponseDto(id, member.getEmail(), member.getPassword());
   }
 
