7ce2de102cb5a53584f0e1be88025622706599a7
eastgyu11 <eastgyu11>
2025-07-29 12:29:36 +0900

    feat: order엔티티 id생성 전략 지정

--- src/main/java/gift/entity/Order.java
+++ src/main/java/gift/entity/Order.java
@@ -3,6 +3,7 @@ package gift.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 public class Order {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column(nullable = false)
